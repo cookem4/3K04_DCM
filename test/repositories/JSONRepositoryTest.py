a386f978ef6dae5c03fec2c1be4dd5941b4932d2
pdkary <pdkary@gmail.com>
2019-10-26 15:44:28 -0400

    added test to json repository

--- test/repositories/JSONRepositoryTest.py
+++ test/repositories/JSONRepositoryTest.py
@@ -1,6 +1,6 @@
+import json
 import os
 import unittest
-import json
 
 from main.repositories.JSONRepository import JSONRepository
 
@@ -8,7 +8,9 @@ FILENAME: str = "testrepo.txt"
 
 
 class JSONRepositoryTest(unittest.TestCase):
-    TEST_STRING: str = "{\"test\": \"TEST TEST TEST\"}"
+    TEST_STRING_ONE: str = "{\"test\": \"TEST TEST TEST\"}"
+    TEST_STRING_TWO: str = "{\"another\": \"TEST TEST TEST\"}"
+
     sut: JSONRepository = JSONRepository(FILENAME)
 
     def setUpClass() -> None:
@@ -20,12 +22,20 @@ class JSONRepositoryTest(unittest.TestCase):
 
     def test_get_returns_empty_dict_when_file_is_empty(self) -> None:
         text_repo = self.sut.get()
-        self.assertEqual(text_repo,{})
+        self.assertEqual(text_repo, {})
 
     def test_save_saves_string_value_into_repo(self) -> None:
-        self.sut.save(self.TEST_STRING)
+        self.sut.save(self.TEST_STRING_ONE)
         result = self.sut.get()
-        self.assertEqual(result, json.loads(self.TEST_STRING))
+        self.assertEqual(result, json.loads(self.TEST_STRING_ONE))
+
+    def test_save_replaces_existing_data(self) -> None:
+        self.sut.save(self.TEST_STRING_ONE)
+        result1 = self.sut.get()
+        self.sut.save(self.TEST_STRING_TWO)
+        result2 = self.sut.get()
+        self.assertNotEqual(result1, result2)
+        self.assertEqual(result2, json.loads(self.TEST_STRING_TWO))
 
 
 if __name__ == '__main__':
